a9eaf58f951ec3ff6231ba8cff1e3f07786d69fb
guusdias <gustavohd34@gmail.com>
2025-03-01 00:45:11 -0300

    fix: reseting project

--- server.ts
+++
@@ -1,8 +0,0 @@
-import app from './src/app';
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-  console.log(`Documentação da API em http://localhost:${PORT}/api-docs`);
-});
